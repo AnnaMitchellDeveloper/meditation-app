12eae02a1298fe6178aadb5398946dba581ce2b5
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-21 15:37:14 -0400

    Created script in JS for play button

---
+++ app.js
@@ -0,0 +1,40 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker buttons");
+
+  //Time display
+  const timeDisplay = document.querySelector(".time-display");
+
+  //Get the Length of the outline
+  const outlineLength = outline.getTotalLength();
+  ///Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //Play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //Create a function specific to stop and play the sound
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+		video.play();
+		play.src = "./svg/pause.svg";
+    }
+	 else {
+		song.pause();
+		video.pause();
+		play.src = "./svg/play.svg";
+	 }
+  };
+};
+app();
